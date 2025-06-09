f282bc384a9124bc8e28e820dbdea70302ea79e4
Man Wai Winnie Yeung <vionwinnie@gmail.com>
2025-06-08 22:32:45 -0700

    Added Deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-# Build and export static site
+# Build static site (Next.js will output to out/ with output: 'export')
 GITHUB_PAGES=true npm run export
 
 # Deploy to gh-pages branch
 if ! command -v gh-pages &> /dev/null; then
-  npm install -g gh-pages
+  npm install --save-dev gh-pages
 fi
 
 gh-pages -d out
